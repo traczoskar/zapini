ac621207c8b825ff74ae4841d65cb4f891726f86
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-08-26 16:36:57 +0200

    chore: Update ActionButtons component with new background color and opacity

--- src/app/ui/action-buttons.tsx
+++ src/app/ui/action-buttons.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const ActionButtons: React.FC = () => {
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 flex gap-8 ">
-      <button className="flex gap-3 items-center bg-sky-700 hover:bg-sky-500 drop-shadow-md  transition hover:scale-105 text-xl text-white font-bold py-4 px-6 rounded-3xl">
+      <button className="flex gap-3 items-center bg-neutral-700 bg-opacity-50 hover:bg-sky-500 drop-shadow-md  transition hover:scale-105 text-xl text-white font-bold py-4 px-6 rounded-3xl">
         Zamów z dostawą
         <Image
           src="/delivery.svg"
