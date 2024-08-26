8fd2d306dc14191136088a104746931d19f473a5
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-08-26 14:29:26 +0200

    feat: Add Footer component for website footer

---
+++ src/app/ui/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="fixed bottom-0 w-full bg-gray-200">
+      <div className="container mx-auto py-4 px-8">Footer</div>
+    </footer>
+  );
+};
+
+export default Footer;
