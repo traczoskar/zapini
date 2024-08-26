c99f92d7c478b39e801ae93b0db46ae52a922ba7
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-08-26 14:29:22 +0200

    chore: Update landing page with new header, action buttons, and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
 import Image from "next/image";
 import Navbar from "./ui/navbar";
 import ActionButtons from "./ui/action-buttons";
+import Footer from "./ui/footer";
 
 export default function Home() {
   return (
-    <header className="w-full relative">
-      <h1 className="absolute w-1 h-1 overflow-hidden clip-rect-0">
-        Zapini - zapiekanki i inne ciulstwa
-      </h1>
-      <Navbar />
-      <Image
-        src="/background.jpg"
-        alt="Vercel Logo"
-        layout="responsive"
-        width={16}
-        height={9}
-      />
-      <ActionButtons />
-    </header>
+    <>
+      <header className="w-full relative">
+        <h1 className="absolute w-1 h-1 overflow-hidden clip-rect-0">
+          Zapini - Najlepsze zapiekanki w Słupsku
+        </h1>
+        <Navbar />
+        <Image
+          src="/background.jpg"
+          alt="Vercel Logo"
+          layout="responsive"
+          width={16}
+          height={9}
+        />
+        <ActionButtons />
+      </header>
+      <main>
+        <Footer />
+      </main>
+    </>
   );
 }
