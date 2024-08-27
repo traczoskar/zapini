123b12546e0416205074d54dbcf786a9cb15479f
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-08-27 13:51:16 +0200

    refactor: Update navbar logo and contact phone number

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -5,9 +5,16 @@ const Navbar: React.FC = () => {
   return (
     <nav className="absolute flex justify-between max-w-[1100px] w-full right-1/2 translate-x-1/2 top-8">
       <Link href="/">
-        <Image
+        {/* <Image
           className="hover:scale-105 transition-transform drop-shadow-md "
           src="/zapini_logo.png"
+          alt="Zapini logo"
+          width={200}
+          height={100}
+        /> */}
+        <Image
+          className="hover:scale-105 transition-transform "
+          src="/zapini_nobg.png"
           alt="Zapini logo"
           width={200}
           height={100}
@@ -21,6 +28,12 @@ const Navbar: React.FC = () => {
         </li>
         <li className="hover:text-sky-600 hover:scale-105 transition px-2 py-2">
           <Link href="#kontakt">Kontakt</Link>
+        </li>
+        <li className="hover:text-sky-600 hover:scale-105 transition px-2 py-2">
+          <a href="tel:+48535630040" className="flex gap-3 hover:text-sky-600">
+            <Image src="/phone.svg" alt="Telefon" width={20} height={20} />
+            535 630 040
+          </a>
         </li>
       </ul>
     </nav>
