d2af34a8a9fd301afb8b1b3368167e90fa07e27e
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-09-14 13:34:45 +0200

    split action buttons for 2 separate buttons

--- src/app/ui/action-buttons.tsx
+++ src/app/ui/action-buttons.tsx
@@ -1,30 +1,38 @@
+import { Inter } from "next/font/google";
 import Image from "next/image";
 
-const ActionButtons: React.FC = () => {
+const inter = Inter({ subsets: ["latin"] });
+
+export const DeliveryButton: React.FC = () => {
   return (
-    <div className="flex flex-col items-center gap-8 ">
-      <button className="flex gap-3 items-center bg-neutral-700 bg-opacity-50 hover:bg-sky-500 drop-shadow-md  transition hover:scale-105 text-xl text-white font-bold py-4 px-6 rounded-3xl">
-        Zamów z dostawą
-        <Image
-          src="/delivery.svg"
-          alt="Zamów z dostawą"
-          width={35}
-          height={35}
-          className="text-white"
-        />
-      </button>
-      <button className="flex gap-3 items-center bg-sky-700 hover:bg-sky-500 drop-shadow-md transition hover:scale-105 text-xl text-white font-bold py-4 px-6 rounded-3xl">
-        Zamów z odbiorem
-        <Image
-          src="/take-away.svg"
-          alt="Zamów z odbiorem"
-          width={30}
-          height={30}
-          className="text-white"
-        />
-      </button>
-    </div>
+    <button
+      className={` ${inter.className} flex gap-3 items-center bg-neutral-700 bg-opacity-50 hover:bg-[#C6E1EC] hover:text-neutral-700 drop-shadow-md  transition hover:scale-105 text-xl text-white font-bold py-4 px-6 rounded-3xl`}
+    >
+      Zamów z dostawą
+      <Image
+        src="/icons/delivery.svg"
+        alt="Zamów z dostawą"
+        width={35}
+        height={35}
+        className="text-white"
+      />
+    </button>
   );
 };
 
-export default ActionButtons;
+export const PickupButton: React.FC = () => {
+  return (
+    <button
+      className={` ${inter.className} flex gap-3 items-center bg-neutral-700 bg-opacity-50 hover:bg-[#C6E1EC] hover:text-neutral-700 drop-shadow-md transition hover:scale-105 text-xl text-white font-bold py-4 px-6 rounded-3xl`}
+    >
+      Zamów z odbiorem
+      <Image
+        src="/icons/take-away.svg"
+        alt="Zamów z odbiorem"
+        width={30}
+        height={30}
+        className="text-white"
+      />
+    </button>
+  );
+};
