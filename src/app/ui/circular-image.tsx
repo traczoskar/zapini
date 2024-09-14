0c6ea37a9c59dfdbac4eb72a62fa212b6cec4c0c
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-09-14 21:54:11 +0200

    added responsive picture sizes to circular-image

--- src/app/ui/circular-image.tsx
+++ src/app/ui/circular-image.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const CircularImage = ({ src, alt }: { src: string; alt: string }) => {
   return (
-    <div className="w-64 h-64 overflow-hidden drop-shadow-md rounded-full">
+    <div className="w-48 h-48 lg:w-64 lg:h-64 overflow-hidden drop-shadow-md rounded-full">
       <Image
         src={src}
         alt={alt}
