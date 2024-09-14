fa43f655b19fee2c41507b552b0f7ca28257b357
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-09-14 13:35:09 +0200

    changed NavBar styling

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -3,37 +3,24 @@ import Link from "next/link";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="absolute flex justify-between max-w-[1100px] w-full right-1/2 translate-x-1/2 top-8">
+    <nav className="flex justify-between max-w-[1100px] w-full mx-auto py-4">
       <Link href="/">
-        {/* <Image
-          className="hover:scale-105 transition-transform drop-shadow-md "
-          src="/zapini_logo.png"
-          alt="Zapini logo"
-          width={200}
-          height={100}
-        /> */}
         <Image
-          className="hover:scale-105 transition-transform "
-          src="/zapini_nobg.png"
+          className="hover:scale-105 transition-transform py-6 "
+          src="/zapini_header.png"
           alt="Zapini logo"
-          width={200}
-          height={100}
+          width={300}
+          height={150}
         />
       </Link>
-      <ul className="flex gap-8 text-xl font-semibold text-neutral-700 items-center">
-        <li className="hover:text-sky-600 hover:scale-105 transition px-2 py-2">
+      <ul className="flex gap-8 text-4xl font-bold tracking-tight text-neutral-500 items-center">
+        <li className="hover:text-sky-600 hover:scale-105 text-[60px] transition ">
           <Link href="/menu.pdf" target="_blank" rel="noopener noreferrer">
-            Menu
+            menu
           </Link>
         </li>
-        <li className="hover:text-sky-600 hover:scale-105 transition px-2 py-2">
-          <Link href="#kontakt">Kontakt</Link>
-        </li>
-        <li className="hover:text-sky-600 hover:scale-105 transition px-2 py-2">
-          <a href="tel:+48535630040" className="flex gap-3 hover:text-sky-600">
-            <Image src="/phone.svg" alt="Telefon" width={20} height={20} />
-            535 630 040
-          </a>
+        <li className="hover:text-sky-600 hover:scale-105 transition pt-3 ">
+          <a href="tel:+48535630040">535 630 040</a>
         </li>
       </ul>
     </nav>
