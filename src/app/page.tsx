cab9ad64f3df4dd3eef3bfc5f64522ba2b32d56a
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-09-14 13:34:33 +0200

    changed styling of main homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,47 @@
 import Image from "next/image";
 import Navbar from "./ui/navbar";
-import ActionButtons from "./ui/action-buttons";
+import { DeliveryButton, PickupButton } from "./ui/action-buttons";
 import Footer from "./ui/footer";
 import Products from "./ui/products";
 
 export default function Home() {
   return (
     <>
-      <header className="w-full relative">
+      <header className="w-full h-[60vh] max-w-[1100px] relative">
         <h1 className="absolute w-1 h-1 overflow-hidden clip-rect-0">
           Zapini - Najlepsze zapiekanki w Słupsku
         </h1>
         <Navbar />
+        <div className="absolute bottom-0 flex max-w-[1100px] w-full pb-5">
+          <DeliveryButton />
+        </div>
       </header>
-      <main className="w-full max-w-[1100px] flex flex-col items-center">
-        <div className="h-96 flex w-full items-center py-96">
+      <main className="w-full flex flex-col items-center bg-secondary ">
+        <div className="relative w-full flex flex-col items-center">
           <Image
-            className="absolute top-40 right-0 z-10"
-            src="/sandwich_nobg.png"
+            className="absolute bottom-0 right-0 z-10 pointer-events-none"
+            src="/sandwich_header.png"
             alt="Tło strony"
-            width={1000}
+            width={1400}
             height={300}
             priority
           />
-          {/* <Image
-          src="/background_2.jpg"
-          alt="Tło strony"
-          layout="responsive"
-          width={16}
-          height={9}
-        /> */}
-          <ActionButtons />
+          <div className=" h-[40vh] max-w-[1100px] pt-5 w-full">
+            <PickupButton />
+          </div>
         </div>
-        <Products />
+        <div className="w-full h-screen bg-gradient-to-b from-secondary to-primary flex flex-col items-center">
+          <div className="w-full max-w-[1100px]">
+          <Products />
+          <Footer />
+          </div>
+
+          
+        </div>
+
+        
       </main>
-      <Footer />
+    
     </>
   );
 }
