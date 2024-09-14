f7d6561f6e88cf67160ea14e3da40c8f98207534
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-09-14 21:54:54 +0200

    changed styling to apply responsiveness

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -3,17 +3,17 @@ import Link from "next/link";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="flex justify-between max-w-[1100px] w-full mx-auto py-4">
-      <Link href="/">
+    <nav className="flex flex-col md:flex-row gap-8 md:gap-0 justify-between items-center lg:max-w-[1100px] w-full mx-auto py-4">
+      <Link href="/" className="w-1/2 md:w-auto flex justify-center">
         <Image
-          className="hover:scale-105 transition-transform py-6 "
+          className="hover:scale-105  transition-transform py-6 "
           src="/zapini_header.png"
           alt="Zapini logo"
           width={300}
           height={150}
         />
       </Link>
-      <ul className="flex gap-8 text-4xl font-bold tracking-tight text-neutral-500 items-center">
+      <ul className="flex flex-col md:flex-row gap-8 text-4xl font-bold tracking-tight text-neutral-500 items-center">
         <li className="hover:text-sky-600 hover:scale-105 text-[60px] transition ">
           <Link href="/menu.pdf" target="_blank" rel="noopener noreferrer">
             menu
