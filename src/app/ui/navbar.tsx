2956375f066b6481175ab524f0f1cd7226f49505
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-09-19 08:36:12 +0200

    minor styling changes for navbar file

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -3,23 +3,24 @@ import Link from "next/link";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="flex flex-col md:flex-row gap-8 md:gap-0 justify-between items-center lg:max-w-[1100px] w-full mx-auto py-4">
-      <Link href="/" className="w-1/2 md:w-auto flex justify-center">
+    <nav className="flex flex-col md:flex-row gap-8 md:gap-0 justify-between items-center lg:max-w-[1200px] w-full mx-auto py-4">
+      <Link href="/" className="w-1/2 md:w-auto flex justify-center ">
         <Image
-          className="hover:scale-105  transition-transform py-6 "
+          className="hover:scale-105  md:w-[50vw] lg:w-[15vw] transition-transform py-6 "
           src="/zapini_header.png"
           alt="Zapini logo"
           width={300}
           height={150}
+          sizes="100vw"
         />
       </Link>
-      <ul className="flex flex-col md:flex-row gap-8 text-4xl font-bold tracking-tight text-neutral-500 items-center">
+      <ul className="flex flex-col md:flex-row gap-4 text-4xl  font-[500] md:font-semibold tracking-tight text-[#555555] items-center">
         <li className="hover:text-sky-600 hover:scale-105 text-[60px] transition ">
           <Link href="/menu.pdf" target="_blank" rel="noopener noreferrer">
             menu
           </Link>
         </li>
-        <li className="hover:text-sky-600 hover:scale-105 transition pt-3 ">
+        <li className="hover:text-sky-600 hover:scale-105 transition font-[600] md:font-bold pt-3 ">
           <a href="tel:+48535630040">535 630 040</a>
         </li>
       </ul>
