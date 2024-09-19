5e3e3efb1e40e02bd8d15b482ec5bdf7019df6c4
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-09-19 14:19:31 +0200

    switched mainPage component to be not visible in place of ComingSoon component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import Image from "next/image";
-import Navbar from "./ui/navbar";
-import { DeliveryButton, PickupButton } from "./ui/action-buttons";
-import Footer from "./ui/footer";
-import Products from "./ui/products";
+import ComingSoon from "./ui/home/coming-soon";
 import MainPage from "./ui/home/main-page";
 
 export default function Home() {
   return (
     <>
-      <MainPage />
+      {/* <MainPage /> */}
+      <ComingSoon />
     </>
   );
 }
