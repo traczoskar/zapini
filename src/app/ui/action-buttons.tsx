0d0161d5e006f89ef9f9d95e7fe1069d3fe72bfc
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-09-19 14:20:29 +0200

    added semantic tags and improved accessibility of buttons

--- src/app/ui/action-buttons.tsx
+++ src/app/ui/action-buttons.tsx
@@ -1,42 +1,37 @@
 import { Inter } from "next/font/google";
-import Image from "next/image";
 import DeliveryIcon from "/public/icons/delivery.svg";
 import PickupIcon from "/public/icons/take-away.svg";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const DeliveryButton: React.FC = () => {
+export const DeliveryButton = ({ href }: { href?: string }) => {
   return (
-    <button
-      className={` ${inter.className} flex gap-3 items-center bg-neutral-700 bg-opacity-50 hover:bg-[#C6E1EC] hover:text-neutral-700 drop-shadow-md  transition hover:scale-105  text-base md:text-xl text-white hover:text-neutral-700 font-bold  px-4 py-3 md:py-4 md:px-6 rounded-3xl z-20`}
+    <a
+      href={href}
+      title="Zamów z dostawą"
+      aria-label="Zamów z dostawą"
+      target="_blank"
+      rel="noopener noreferrer"
+      className={` ${inter.className} w-56 md:w-72 flex gap-4 justify-center items-center bg-neutral-700 bg-opacity-50 hover:bg-[#C6E1EC] drop-shadow-md  transition hover:scale-105  text-base md:text-xl text-white hover:text-neutral-700 font-bold  px-4 py-3 md:py-4 md:px-6 rounded-3xl z-20`}
     >
       Zamów z dostawą
-      {/* <Image
-        src="/icons/delivery.svg"
-        alt="Zamów z dostawą"
-        width={35}
-        height={35}
-        className="text-white"
-      /> */}
       <DeliveryIcon className="md:w-8 md:h-8 w-6 h-6" />
-    </button>
+    </a>
   );
 };
 
-export const PickupButton: React.FC = () => {
+export const PickupButton = ({ href }: { href?: string }) => {
   return (
-    <button
-      className={` ${inter.className} flex gap-3 items-center bg-neutral-700 bg-opacity-50 hover:bg-[#f6DCC8] hover:text-neutral-700 drop-shadow-md transition hover:scale-105 text-base md:text-xl text-white font-bold px-4 py-3 md:py-4 md:px-6 rounded-3xl z-20`}
+    <a
+      href={href}
+      title="Zamów z odbiorem"
+      aria-label="Zamów z odbiorem"
+      target="_blank"
+      rel="noopener noreferrer"
+      className={` ${inter.className} w-56 md:w-72 flex gap-4 justify-center bg-neutral-700 bg-opacity-50 hover:bg-[#f6DCC8] hover:text-neutral-700 drop-shadow-md transition hover:scale-105 text-base md:text-xl text-white font-bold px-4 py-3 md:py-4 md:px-6 rounded-3xl z-20`}
     >
       Zamów z odbiorem
-      {/* <Image
-        src="/icons/take-away.svg"
-        alt="Zamów z odbiorem"
-        width={30}
-        height={30}
-        className="text-white"
-      /> */}
       <PickupIcon className="md:w-8 md:h-8 w-6 h-6" />
-    </button>
+    </a>
   );
 };
