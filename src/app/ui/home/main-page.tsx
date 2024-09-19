e342865a8124643186202284c89622c1727392e8
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-09-19 14:21:29 +0200

    minor fixes in MainPage component

--- src/app/ui/home/main-page.tsx
+++ src/app/ui/home/main-page.tsx
@@ -6,19 +6,18 @@ import Products from "../products";
 
 export default function MainPage() {
   return (
-    <>
+    <div className="overflow-x-hidden w-full flex flex-col items-center">
       <header className="w-full h-[65vh] md:h-[60vh] max-w-[1200px] relative">
         <h1 className="absolute w-1 h-1 overflow-hidden clip-rect-0">
-          Zapini - Najlepsze zapiekanki w Słupsku
+          Zapini - Pyszne śniadania w Słupsku | Bajgle, zapiekanki, bagietki
         </h1>
         <Navbar />
         <div className="absolute bottom-0 max-w-[1200px] w-full pb-3 md:pb-5  flex justify-center items-end md:justify-start">
-          <DeliveryButton />
+          <DeliveryButton href="https://zamow.zapini.pl" />
         </div>
       </header>
-      <main className="w-full flex flex-col items-center bg-secondary ">
+      <main className="w-full flex flex-col items-center bg-secondary  ">
         <div className="relative w-full flex flex-col items-center">
-          {/* OVERFLOW USTAWIĆ ŻEBY NIE BYŁO MOŻNA PRZESUWAĆ W POZIOMIE */}
           <Image
             className="overflow-hidden absolute -bottom-10 -right-12 md:-bottom-12 md:right-0 z-10 pointer-events-none drop-shadow-lg w-[140vw] md:w-[70vw] h-auto max-w-[1200px]"
             src="/bagel-header.png"
@@ -28,9 +27,8 @@ export default function MainPage() {
             priority
             sizes="100vw"
           />
-          {/* <div className="absolute pointer-events-none -inset-12 bg-gradient-to-b from-transparent via-transparent to-secondary opacity-100 z-20" /> */}
           <div className="h-[35vh] md:h-[40vh] max-w-[1200px] pt-3 md:pt-5 w-full flex justify-center items-start md:justify-start">
-            <PickupButton />
+            <PickupButton href="https://zamow.zapini.pl" />
           </div>
         </div>
         <div className="w-full h-[8vh] md:h-[20vh] bg-gradient-to-b from-secondary to-primary flex flex-col items-center " />
@@ -43,6 +41,6 @@ export default function MainPage() {
           </div>
         </div>
       </main>
-    </>
+    </div>
   );
 }
