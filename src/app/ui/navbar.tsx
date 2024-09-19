98aaa8b8af3868c7953644706e2b75e3906368f6
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-09-19 14:20:55 +0200

    minor styling changes in NavBar component

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -4,7 +4,11 @@ import Link from "next/link";
 const Navbar: React.FC = () => {
   return (
     <nav className="flex flex-col md:flex-row gap-8 md:gap-0 justify-between items-center lg:max-w-[1200px] w-full mx-auto py-4">
-      <Link href="/" className="w-1/2 md:w-auto flex justify-center ">
+      <Link
+        href="/"
+        className="w-1/2 md:w-auto flex justify-center "
+        title="Strona Główna"
+      >
         <Image
           className="hover:scale-105  md:w-[50vw] lg:w-[15vw] transition-transform py-6 "
           src="/zapini_header.png"
@@ -16,7 +20,12 @@ const Navbar: React.FC = () => {
       </Link>
       <ul className="flex flex-col md:flex-row gap-4 text-4xl  font-[500] md:font-semibold tracking-tight text-[#555555] items-center">
         <li className="hover:text-sky-600 hover:scale-105 text-[60px] transition ">
-          <Link href="/menu.pdf" target="_blank" rel="noopener noreferrer">
+          <Link
+            href="/menu"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Menu Zapini - Aktualna oferta"
+          >
             menu
           </Link>
         </li>
