a2c7afe7d8dc4270aab6620ea9d51860b3ff4f6c
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-09-19 08:36:01 +0200

    minor styling changes for footer  file

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 const Footer: React.FC = () => {
   return (
-    <footer className={`${inter.className} text-neutral-800 md:py-12 py-6 w-full `}>
-      <div className="container md:mx-auto flex flex-col md:flex-row md:gap-36 gap-12 items-center md:max-w-[1100px]">
+    <footer
+      className={`${montserrat.className} text-[#555555] md:py-12 py-6 pt-10 w-full bg-secondary `}
+    >
+      <div className="container md:mx-auto flex flex-col md:flex-row md:gap-36 gap-12 items-center md:items-start md:max-w-[1100px]">
         {/* Logo section */}
         <div className="flex items-center">
           <Image
-            src="/zapini_header.png"
+            src="/zapini_white_nobg.png"
             alt="Zapini logo"
             width={200}
             height={100}
@@ -19,7 +21,7 @@ const Footer: React.FC = () => {
         </div>
 
         {/* Contact section */}
-        <address className="flex flex-col gap-4 not-italic">
+        <address className="flex flex-col gap-4 not-italic items-center">
           <h3 className="font-semibold mb-2">Kontakt</h3>
           <a href="tel:+48535630040" className="flex gap-4 hover:text-sky-600">
             <Image
@@ -32,8 +34,9 @@ const Footer: React.FC = () => {
           </a>
           <a
             href="mailto:kontakt@zapini.pl"
-            className="flex gap-4 hover:text-sky-600"
+            className="flex gap-4 hover:text-sky-600 mb-3"
           >
+            {/* IKONY W ANTRACYCIE*/}
             <Image
               src="/icons/email.svg"
               alt="Telefon"
@@ -58,6 +61,7 @@ const Footer: React.FC = () => {
               href="https://www.instagram.com"
               className="hover:bg-sky-600 bg-sky-800 p-2 rounded-full "
             >
+              {/* LOGO INSTAGRAM W ORYGINALNYCH KOLORACH */}
               <Image
                 src="/icons/instagram.svg"
                 alt="Instagram"
@@ -69,15 +73,15 @@ const Footer: React.FC = () => {
         </address>
 
         {/* Opening hours */}
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col md:gap-4 gap-1 items-center">
           <h3 className="font-semibold mb-2">Godziny otwarcia</h3>
-          <span>Pon-Pt: 9:00 - 17:00</span>
-          <span>Sob: 10:00 - 14:00</span>
+          <span>Pon-Pt: 9:00 - 16:00</span>
+          <span>Sob: 09:00 - 14:00</span>
           <span>Nd: Zamknięte</span>
         </div>
 
         {/* Address */}
-        <address className="flex flex-col gap-4 not-italic">
+        <address className="flex flex-col gap-1 md:gap-4 not-italic items-center">
           <h3 className="font-semibold mb-2">Adres</h3>
           <span>ul. Bema 14</span>
           <span>76-200 Słupsk</span>
