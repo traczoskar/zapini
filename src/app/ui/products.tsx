70c158d7d1b47defcf6ebaf69fc3dd288799d200
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-09-19 14:21:12 +0200

    replaced products pictures to the newest ones

--- src/app/ui/products.tsx
+++ src/app/ui/products.tsx
@@ -1,4 +1,4 @@
-import CircularImage from "./circular-image";
+import Image from "next/image";
 import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
@@ -7,35 +7,47 @@ const Products: React.FC = () => {
   const products = [
     {
       name: "Bajgle na Okrągło",
-      image: "/products/bagel.jpg",
+      image: "/products/bajgiel.png",
       description:
         "Puszyste, chrupiące bajgle z różnorodnymi dodatkami, idealne na każdą porę dnia.",
     },
     {
       name: "Bagietki z Charakterem",
-      image: "/products/sandwich.jpg",
+      image: "/products/bagietka.png",
       description:
         "Chrupiące bagietki z wyrazistymi smakami, które rozbudzą Twoje kubki smakowe.",
     },
     {
       name: "Zapiekanki z Pasją",
-      image: "/products/toast.jpg",
+      image: "/products/zapiekanka.png",
       description:
         "Klasyczne zapiekanki przygotowane z pasją, wypełnione smakowitymi składnikami i zapieczone do perfekcji.",
     },
   ];
 
   return (
-    <div className="flex flex-col items-center container pt-10 md:pb-40 pb-10">
+    <div className="flex flex-col items-center container pt-10 md:pt-14 md:pb-40 pb-10">
       <ul className="w-full flex flex-col md:flex-row justify-between gap-12">
         {products.map((product, index) => (
           <li
             key={index}
-            className={`${montserrat.className} flex flex-col gap-6 items-center `}
+            className={`${montserrat.className} flex flex-col items-center `}
           >
-            <CircularImage src={product.image} alt={product.name} />
-            <h3 className="font-semibold text-xl md:text-2xl text-center">{product.name}</h3>
-            <p className="text-center md:text-base px-4 md:px-0 text-sm w-64">{product.description}</p>
+            <div className={` flex items-center justify-center`}>
+              <Image
+                src={product.image}
+                alt={product.name}
+                width={250}
+                height={250}
+                className="drop-shadow-xl"
+              />
+            </div>
+            <h3 className="font-semibold text-xl md:text-2xl text-center -mt-6 mb-4">
+              {product.name}
+            </h3>
+            <p className="text-center md:text-base px-4 md:px-0 text-sm w-80">
+              {product.description}
+            </p>
           </li>
         ))}
       </ul>
