d18611d0cbb6f5009b98b0058702b600ffe92ac7
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-09-19 14:18:49 +0200

    updated Metadata object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,23 +5,83 @@ import "./globals.css";
 const leagueSpartan = League_Spartan({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Zapini",
-  description: "Najlepsze zapiekanki w mieście",
+  title: {
+    default:
+      "Zapini - Pyszne śniadania w Słupsku | Bajgle, zapiekanki, bagietki",
+    template: "%s | Zapini - Pyszne śniadania w Słupsku",
+  },
+  description:
+    "Odkryj smaczne śniadania w Słupsku! Zapini oferuje świeże bajgle, zapiekanki oraz bagietki i pyszną kawę z dostawą lub odbiorem własnym. Zamów online już teraz!",
   icons: "/icon.png",
-  authors: { name: "Oskar Tracz", url: "https://traczoskar.dev" },
+  authors: [
+    { name: "Oskar Tracz", url: "https://traczoskar.dev" },
+    { name: "Zapini", url: "https://zapini.pl" },
+  ],
+  creator: "Zapini",
+  publisher: "Zapini",
+  metadataBase: new URL("https://www.zapini.pl"),
   keywords: [
     "zapiekanki",
     "słupsk",
+    "bajgle",
+    "bagietki",
+    "bagietki śniadaniowe",
+    "śniadanie słupsk",
+    "restauracja śniadaniowa słupsk",
+    "dostawa śniadań słupsk",
+    "dostawa śniadań",
     "zapini",
     "panini",
-    "śniadania słupsk",
+    "śniadania Słupsk",
     "śniadania",
     "kawa słupsk",
     "kawa",
     "kanapki",
   ],
-  alternates: { canonical: "https://www.zapini.pl" },
-  robots: "index, follow",
+  openGraph: {
+    type: "website",
+    url: "https://www.zapini.pl",
+    title: "Zapini - Pyszne śniadania w Słupsku | Bajgle, zapiekanki, bagietki",
+    description:
+      "Odkryj smaczne śniadania w Zapini! Świeże bajgle, zapiekanki oraz bagietki i pyszna kawa z dostawą lub odbiorem własnym w Słupsku. Zamów online!",
+    siteName: "Zapini",
+    images: [
+      {
+        url: "https://www.zapini.pl/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Zapini - Pyszne śniadania w Słupsku | Bajgle, zapiekanki, bagietki",
+      },
+    ],
+    locale: "pl_PL",
+  },
+  alternates: {
+    canonical: "/",
+    types: {
+      "application/xml": [
+        { url: "https://www.zapini.pl/sitemap.xml", title: "Sitemap" },
+      ],
+    },
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Zapini - Pyszne śniadania w Słupsku | Bajgle, zapiekanki, bagietki",
+    description:
+      "Zapini | Świeże bajgle, zapiekanki i bagietki z dostawą lub odbiorem własnym. Zamów online!",
+    images: ["https://www.zapini.pl/og-image.png"],
+  },
+  category: "Restauracja",
 };
 
 export default function RootLayout({
@@ -30,7 +90,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pl">
+    <html lang="pl" className="scroll-smooth">
       <body
         className={`${leagueSpartan.className} antialiased min-h-screen flex flex-col items-center w-full`}
       >
