8b3e53681625f03bd41ce33bfd2997fb09d4bcd3
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-10-01 11:38:53 +0200

    deleted unused line of code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import ComingSoon from "./ui/home/coming-soon";
 import MainPage from "./ui/home/main-page";
 
 export default function Home() {
   return (
     <>
-      {/* <MainPage /> */}
-      <ComingSoon />
+      <MainPage />
     </>
   );
 }
